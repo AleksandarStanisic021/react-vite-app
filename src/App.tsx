c89e1dd1a1181e12b88f061a9908b99e48b63a9f
Aleksandar Stanisic <ciandre23@gmail.com>
2026-07-14 00:50:42 +0200

    testing app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p>Welcome to Vite!</p>
+      <h2>Welcome to Vite!</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
+        quaerat nesciunt, nulla natus, vero obcaecati architecto corporis
+        reiciendis ipsum eos quas ratione eveniet quae minus deserunt et.
+        Officia, laudantium unde!
+      </p>
     </>
   );
 }
